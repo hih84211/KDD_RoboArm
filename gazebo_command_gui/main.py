0270f8bf665ff5a41589099d06316e5d57523c4e
hih84211 <30366147+hih84211@users.noreply.github.com>
2021-10-30 22:25:48 +0800

    Add files via upload

---
+++ gazebo_command_gui/main.py
@@ -0,0 +1,38 @@
+import signal
+import sys
+import threading
+
+import rospy
+
+from python_qt_binding.QtWidgets import QApplication
+import jointInfo
+import amazing_gui
+
+
+
+
+if __name__ == '__main__':
+    
+    try:
+        rospy.init_node('command_gui')
+        app = QApplication(sys.argv)
+        app.setApplicationDisplayName("Command Publisher")
+        num_rows = jointInfo.get_param('num_rows', 0)
+        jsp_gui = amazing_gui.JointStatePublisherGui("Node: " + rospy.get_name(),
+                                                                   jointInfo.JointInfo(),
+                                                                   num_rows)
+        jsp_gui.show()
+        jsp_gui.sliderUpdateTrigger.emit()
+
+        threading.Thread(target=jsp_gui.jsp.loop).start()
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
+        sys.exit(app.exec_())
+        
+        
+
+    except rospy.ROSInterruptException:
+        pass
+        
+    except Exception as e:
+        print(e)
+
