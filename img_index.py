7fae3b11981a058904021af9bb58dc2ca0c1e58e
hih84211 <30366147+hih84211@users.noreply.github.com>
2021-11-30 21:23:06 +0800

    Update img_index.py

--- img_index.py
+++ img_index.py
@@ -1,36 +1,67 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import cv2
 import numpy as np
 import math
+import listofpathpoint
 import rospy
 from std_msgs.msg import Float64
 from sensor_msgs.msg import JointState
 import threading
 import time
-import random
-import listofpathpoint
+import yaml
 
-img = cv2.imread('/home/peter/mother_board.png')
+
+img = cv2.imread('path to photo')
 current_pose = [.0, .0, .0]
 # rospy.Publisher('/KDD_RoboArm/nozzlebase_controller/command', Float64, queue_size = 1),
 # rospy.Publisher('/KDD_RoboArm/nozzle_controller/command', Float64, queue_size = 1)]))
 
-def on_EVENT_LBUTTONDOWN(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        xy = (x, img.shape[:2][0]-y)
-        print('index: ', xy)
-        val = img[y][x][0]
-        print('val: ', val)
-        coord = pixel2coordinate(xy, val)
-        print('3D coordinate: ', coord)
-        goto(coord, param)
-        
+class YamlParser:
+    
+    parsed_data = {'path': (list, True)}
+    
+    def __init__(self, inFileName):
+        # read YAML
+        infile = open(inFileName, 'r')
+        ymlcfg = yaml.safe_load(infile)
+        infile.close()
+        '''
+        eccfg = ymlcfg.get(self.sectionName, None)
+        if eccfg is None: raise Exception('Missing {} section in cfg file'.format(self.sectionName))
+        '''
+        for key in self.parsed_data:
+            data = ymlcfg.get(key, None)
+            if type(data) != self.parsed_data[key][0]:
+                raise Exception('Parameter "{}" has wrong type'.format(key))
+            # create attributes on the fly
+            setattr(self, key, data)
+
+def on_EVENT_LBUTTONDOWN( x, y, param):
+    xy = (x, img.shape[:2][0]-y)
+    print('index: ', xy)
+    val = img[y][x][0]
+    print('val: ', val)
+    coord = pixel2coordinate(xy, val)
+    print('3D coordinate: ', coord)
+    goto(coord, param)
+    '''val = img[y][x][0]
+    print('val: ', val)
+    coord = pixel2coordinate(xy, val)
+    print('3D coordinate: ', coord)
+    param[0].publish(coord[0])
+    param[1].publish(coord[1])
+    param[2].publish(coord[2] - 0.03)
+    cv2.circle(img, (x, y), 1, (255, 0, 0), thickness=-1)
+    cv2.putText(img, xy, (x, y), cv2.FONT_HERSHEY_PLAIN,
+                1.0, (0, 0, 0), thickness=1)
+    img.imshow('image', img)'''
 
 def pixel2coordinate(p_index, val):
     # Gazebo中原點座標與height_map原點間的偏量
-    x_offset = -485
-    y_offset = -123
+    x_offset = -460
+    y_offset = -120
     # height_map 中的 0 與真實值之間的偏量
     z_bias = -9.7    
     tf_x = 0.239144/1024
@@ -42,47 +73,33 @@ def pixel2coordinate(p_index, val):
     z = (val+z_bias)*tf_z
     
     return(x, y, z)
-  
-
+    
 def goto(xyz, publishers):
     global current_pose
     publishers[0].publish(xyz[0])
     publishers[1].publish(xyz[1])
-    publishers[2].publish(xyz[2] - 0.045)
-    print('Go to: ', xyz)
-    #time.sleep(0.2)
-    
+    publishers[2].publish(xyz[2] - 0.03)
+    print('current: ', current_pose)
 
 def go_for_a_walk(seq, publishers):
     for i in seq:
-        # val = img[i[1]][img.shape[:2][0]-i[0]][0]
-        print('i[1]: ',i[1])
-        print('i[0]: ',i[0])
-        val = img[int(i[1])][img.shape[:2][0]-int(i[0])][0]
+        val = img[i[0]][i[1]][0]
         coord = pixel2coordinate(i, val)
-        goto(coord, publishers)
         while True:
             goto(coord, publishers)
             if r_we_there(coord):
                 break
-
-
+    
 def r_we_there(target):
-    tolerance = 1e-8
-    err = eucli_dist_sqr(get_position(), target)
-    if  err< tolerance:
-        print('Arrive, err: ', err)
+    tolerance = 1e-4
+    if eucli_dist_sqr(get_position(), target) < tolerance:
         return True
     else:
-        print('Not even close, err: ',err)
         return False
-
 
 def get_position():
     global current_pose
-    print('current: ', current_pose)
     return [current_pose[0], current_pose[1]]
-
 
 def eucli_dist_sqr(p1, p2):
     return (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
@@ -94,39 +111,45 @@ def pose_cb(data):
     # print('listener called!!!!!!!!!!!!!!  ', data.position)
     current_pose = [data.position[0], data.position[1], data.position[2]]
 
-
 def pose_listener():
+    # In ROS, nodes are uniquely named. If two nodes with the same
+    # name are launched, the previous one is kicked off. The
+    # anonymous=True flag means that rospy will choose a unique
+    # name for our 'listener' node so that multiple listeners can
+    # run simultaneously.
+    
     rospy.Subscriber('/KDD_RoboArm/joint_states', JointState, pose_cb)
     rate=rospy.Rate(10)
+    # spin() simply keeps python from exiting until this node is stopped
     
+    
+def job():
+  for i in range(150):
+    print("Child thread:", i)
+    time.sleep(1)
+
 
 if __name__=='__main__':
-    
     rospy.init_node('command_fromImg')
     x_publisher = rospy.Publisher('/KDD_RoboArm/x_position_controller/command', Float64, queue_size = 1)
     y_publisher = rospy.Publisher('/KDD_RoboArm/y_position_controller/command', Float64, queue_size = 1) 
     z_publisher = rospy.Publisher('/KDD_RoboArm/z_position_controller/command', Float64, queue_size = 1)
     
-    '''t = threading.Thread(target = pose_listener)
+    # Remember to change the path value 
+    cfg = YamlParser('./test.cfg')
+    # print(cfg.path)
+    path = cfg.path # get a list of x y tuples
+    for point in path:
+        on_EVENT_LBUTTONDOWN(point[0],point[1],(x_publisher, y_publisher, z_publisher))
+    t = threading.Thread(target = pose_listener)
     t.start()
-    # path = [ (random.randint(0, 1023), random.randint(0, 553)) for i in range(30)]
-    path = listofpathpoint.run() # get a list of x y tuples
-    print(path)
-    
-    go_for_a_walk(path, (x_publisher, y_publisher, z_publisher))
-    '''
-    cv2.namedWindow('image')
-    cv2.setMouseCallback('image', on_EVENT_LBUTTONDOWN, (x_publisher, y_publisher, z_publisher))
-    cv2.imshow('image', img)
-    
     while(True):
         try:
             
             cv2.waitKey(100)
         except Exception:
+            # 等待 t 這個子執行緒結束
             t.join()
-            cv2.destroyWindow('image')
             break
-    
-    cv2.waitKey(0)
-    cv2.destroyAllWindow()
+    # 等待 t 這個子執行緒結束
+    t.join()
